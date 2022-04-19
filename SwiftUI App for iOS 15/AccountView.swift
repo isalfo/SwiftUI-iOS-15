862c3c98af6559db02f7706f427dff0d235134ab
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-19 10:54:03 -0300

    feat: add swipeAction

--- SwiftUI App for iOS 15/AccountView.swift
+++ SwiftUI App for iOS 15/AccountView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AccountView: View {
+  @State var isDeleted: Bool = false
     var body: some View {
       NavigationView {
         List {
@@ -54,13 +55,25 @@ struct AccountView: View {
           .listRowSeparator(.hidden)
           
           Section {
-            Link(destination: URL(string: "https://apple.com")!) {
-              HStack {
-                Label("Website", systemImage: "house")
-                Spacer()
-                Image(systemName: "link")
-                  .foregroundColor(.secondary)
+            if !isDeleted {
+              Link(destination: URL(string: "https://apple.com")!) {
+                HStack {
+                  Label("Website", systemImage: "house")
+                  Spacer()
+                  Image(systemName: "link")
+                    .foregroundColor(.secondary)
+                }
               }
+              .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                Button(action: { isDeleted = true }) {
+                  Label("Delete", systemImage: "trash")
+                }
+                .tint(.red)
+                Button { } label: {
+                  Label("Pin", systemImage: "pin")
+                }
+                .tint(.yellow) 
+            }
             }
             Link(destination: URL(string: "https://youtube.com")!) {
               HStack {
