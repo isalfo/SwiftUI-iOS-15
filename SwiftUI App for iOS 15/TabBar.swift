3d0fbb17a007ee94f5fe950db9caa070cc20ffa8
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-20 11:20:32 -0300

    feat: add tabBar enum and switch between tabs

--- SwiftUI App for iOS 15/TabBar.swift
+++ SwiftUI App for iOS 15/TabBar.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct TabBar: View {
-    var body: some View {
-      ZStack(alignment: .bottom) {
-        ContentView()
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
-        HStack {
-          ForEach(tabItems) { item in
+  @State var selectedTab: Tab = .home
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      
+      Group {
+        switch selectedTab {
+        case .home:
+          ContentView()
+        case .explore:
+          AccountView()
+        case .notifications:
+          AccountView()
+        case .library:
+          AccountView()
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      
+      HStack {
+        ForEach(tabItems) { item in
+          Button {
+            selectedTab = item.tab
+          } label: {
             VStack(spacing: 0) {
               Image(systemName: item.icon)
                 .symbolVariant(.fill)
@@ -25,18 +43,20 @@ struct TabBar: View {
             }
             .frame(maxWidth: .infinity)
           }
+          .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
         }
-        .padding(.horizontal, 8)
-        .padding(.top, 14)
-        .frame(height: 88, alignment: .top)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
-        .strokeStyle(cornerRadius: 34)
-        .frame(maxHeight: .infinity, alignment: .bottom)
-        .ignoresSafeArea()
-        
-        
       }
+      .padding(.horizontal, 8)
+      .padding(.top, 14)
+      .frame(height: 88, alignment: .top)
+      .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+      .strokeStyle(cornerRadius: 34)
+      .frame(maxHeight: .infinity, alignment: .bottom)
+      .ignoresSafeArea()
+      
+      
     }
+  }
 }
 
 struct TabBar_Previews: PreviewProvider {
