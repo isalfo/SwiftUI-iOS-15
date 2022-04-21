da49a5e20e0165ffd1211e18b9b5ba24fdc93a09
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-21 17:44:00 -0300

    feat: add hardcoded courses and horizontal tabView

--- SwiftUI App for iOS 15/Views/HomeView.swift
+++ SwiftUI App for iOS 15/Views/HomeView.swift
@@ -11,32 +11,56 @@ struct HomeView: View {
   @State var hasScrolled: Bool = false
   
     var body: some View {
-      ScrollView {
-        GeometryReader { proxy in
-          Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-        }
-        .frame(height: 0)
-        FeaturedItem()
-        Color.clear.frame(height: 1000)
-      }
-      .coordinateSpace(name: "scroll")
-      .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-        withAnimation(.easeInOut) {
-          if value < 0 {
-            hasScrolled = true
-          } else {
-            hasScrolled = false
-          }
-        }
+      ZStack {
+        Color("Background").ignoresSafeArea()
         
-      })
-      .safeAreaInset(edge: .top, content: {
-        Color.clear.frame(height: 70)
-      })
-      .overlay(
-        NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
+        ScrollView {
+          
+          scrollDetection
+          featured
+          
+          Color.clear.frame(height: 1000)
+        }
+        .coordinateSpace(name: "scroll")
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+          withAnimation(.easeInOut) {
+            if value < 0 {
+              hasScrolled = true
+            } else {
+              hasScrolled = false
+            }
+          }
+          
+        })
+        .safeAreaInset(edge: .top, content: {
+          Color.clear.frame(height: 70)
+        })
+        .overlay(
+          NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
       )
+      }
     }
+  
+  var scrollDetection: some View {
+    GeometryReader { proxy in
+      Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+    }
+    .frame(height: 0)
+  }
+  
+  var featured: some View {
+    TabView {
+      ForEach(courses) { item in
+        FeaturedItem(course: item)
+      }
+    }
+    .tabViewStyle(.page(indexDisplayMode: .never))
+    .frame(height: 430)
+    .background(
+      Image("Blob 1")
+        .offset(x: 250, y: -100)
+    )
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
