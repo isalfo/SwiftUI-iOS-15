a8792f9fe8b09b43ec184ff1492d4616b5b4fbce
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-25 13:59:33 -0300

    feat|improvement: add homeView scrolling state when leaving detail view

--- SwiftUI App for iOS 15/Views/HomeView.swift
+++ SwiftUI App for iOS 15/Views/HomeView.swift
@@ -12,6 +12,7 @@ struct HomeView: View {
   @State var show: Bool = false
   @State var hasScrolled: Bool = false
   @State var showStatusBar: Bool = true
+  @State var selectedID = UUID()
   
     var body: some View {
       ZStack {
@@ -30,17 +31,20 @@ struct HomeView: View {
             .padding(.horizontal, 20)
           
           if !show {
+            
+            cards
+            
+          } else {
             ForEach(courses) { course in
-              CourseItem(namespace: namespace, course: course, show: $show)
-                .onTapGesture {
-                  withAnimation(.openCard) {
-                    show.toggle()
-                    showStatusBar = false
-                  }
-              }
+              Rectangle()
+                .fill(.white)
+                .frame(height: 300)
+                .cornerRadius(30)
+                .shadow(color: Color("Shadow"), radius: 20, x: 0, y: 10)
+                .opacity(0.3)
+              .padding(.horizontal, 30)
             }
           }
-          
         }
         .coordinateSpace(name: "scroll")
         .safeAreaInset(edge: .top, content: {
@@ -50,15 +54,10 @@ struct HomeView: View {
           NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
       )
         if show {
-          ForEach(courses) { course in
-            CourseView(namespace: namespace, course: course, show: $show)
-              .zIndex(1)
-              .transition(.asymmetric(
-                insertion: .opacity.animation(.easeInOut(duration: 0.1)),
-              removal: .opacity.animation(.easeInOut(duration: 0.3).delay(0.2))))
-          }
+          
+          detail
+          
         }
-        
       }
       .statusBar(hidden: !showStatusBar)
       .onChange(of: show) { newValue in
@@ -69,7 +68,6 @@ struct HomeView: View {
             showStatusBar = true
           }
         }
-        
       }
     }
   
@@ -86,7 +84,6 @@ struct HomeView: View {
           hasScrolled = false
         }
       }
-      
     })
   }
   
@@ -117,6 +114,30 @@ struct HomeView: View {
       Image("Blob 1")
         .offset(x: 250, y: -100)
     )
+  }
+  
+  var cards: some View {
+    ForEach(courses) { course in
+      CourseItem(namespace: namespace, course: course, show: $show)
+        .onTapGesture {
+          withAnimation(.openCard) {
+            show.toggle()
+            showStatusBar = false
+            selectedID = course.id
+          }
+      }
+    }
+  }
+  
+  var detail: some View {
+    ForEach(courses) { course in
+      if course.id == selectedID {CourseView(namespace: namespace, course: course, show: $show)
+                      .zIndex(1)
+                      .transition(.asymmetric(
+                        insertion: .opacity.animation(.easeInOut(duration: 0.1)),
+                      removal: .opacity.animation(.easeInOut(duration: 0.3).delay(0.2))))
+      }
+    }
   }
 }
 
