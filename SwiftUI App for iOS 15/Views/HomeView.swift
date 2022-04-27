03daf91ab966715fa3c82f5e7e1b18a95bdea4ce
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-27 11:55:39 -0300

    feat|improve: add modal sheet in a loop

--- SwiftUI App for iOS 15/Views/HomeView.swift
+++ SwiftUI App for iOS 15/Views/HomeView.swift
@@ -13,6 +13,8 @@ struct HomeView: View {
   @State var hasScrolled: Bool = false
   @State var showStatusBar: Bool = true
   @State var selectedID = UUID()
+  @State var showCourse: Bool = false
+  @State var selectedIndex: Int = 0
   @EnvironmentObject var model: Model
   
     var body: some View {
@@ -94,7 +96,7 @@ struct HomeView: View {
   
   var featured: some View {
     TabView {
-      ForEach(featuredCourses) { course in
+      ForEach(Array(featuredCourses.enumerated()), id: \.offset) { index, course in
         GeometryReader { proxy in
           let minX = proxy.frame(in: .global).minX
           
@@ -113,6 +115,10 @@ struct HomeView: View {
                 .offset(x: 32, y: -80)
                 .offset(x: minX / 2)
           )
+            .onTapGesture {
+              showCourse = true
+              selectedIndex = index
+            }
         }
       }
     }
@@ -122,6 +128,9 @@ struct HomeView: View {
       Image("Blob 1")
         .offset(x: 250, y: -100)
     )
+    .sheet(isPresented: $showCourse) {
+      CourseView(namespace: namespace, course: featuredCourses[selectedIndex], show: $showCourse)
+    }
   }
   
   var cards: some View {
