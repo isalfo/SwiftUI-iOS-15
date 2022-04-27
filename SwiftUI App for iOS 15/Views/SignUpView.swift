8f703d97d8eef1bd2f9f63a8c48fb4ea8c7e022a
Gonzalo Alfonso <gonzalo.alfonso@globant.com>
2022-04-27 16:21:34 -0300

    feat: animated focus states

--- SwiftUI App for iOS 15/Views/SignUpView.swift
+++ SwiftUI App for iOS 15/Views/SignUpView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct SignUpView: View {
+  
+  enum Field: Hashable {
+    case email
+    case password
+  }
+  
   @State var email: String = ""
   @State var password: String = ""
+  @FocusState var focusedField: Field?
+  @State var circleY: CGFloat = 120
   
     var body: some View {
       VStack(alignment: .leading, spacing: 16) {
@@ -23,9 +31,13 @@ struct SignUpView: View {
           .keyboardType(.emailAddress)
           .autocapitalization(.none)
           .disableAutocorrection(true)
+          .focused($focusedField, equals: .email)
+          .shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
         SecureField("Password", text: $password)
           .inputStyle(icon: "lock")
           .textContentType(.password)
+          .focused($focusedField, equals: .password)
+          .shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
         Button { } label: {
           Text("Create an account")
             .frame(maxWidth: .infinity)
@@ -55,17 +67,34 @@ struct SignUpView: View {
       }
       .padding(20)
       .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+      .background(
+        Circle().fill(.blue)
+          .frame(width: 68, height: 68)
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+          .offset(y: circleY)
+      )
       .strokeStyle(cornerRadius: 30)
       .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
       .padding(20)
       .background(
         Image("Blob 1").offset(x: 200, y: -100)
       )
+      .onChange(of: focusedField) { value in
+        withAnimation {
+          if value == .email {
+            circleY = 120
+          } else {
+            circleY = 190
+          }
+        }
+      }
     }
 }
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
+      ZStack {
         SignUpView()
+      }
     }
 }
